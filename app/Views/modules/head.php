347378c63a9020e5354a479a93ef1ddd98560fc3
RedyRI <redy.aries12@gmail.com>
2022-06-27 20:34:12 -0500

    eliminando carpeta dist de splide

--- app/Views/modules/head.php
+++ app/Views/modules/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,10 +9,11 @@
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
-  <script src="<?php echo base_url()  ?>/js/splide/dist/js/splide.min.js"></script>
-  <link defer rel="stylesheet" href="<?php echo base_url()  ?>/js/splide/dist/css/splide.min.css">
+  <script src="<?php echo base_url()  ?>/js/splide.min.js"></script>
+  <link defer rel="stylesheet" href="<?php echo base_url()  ?>/css/splide.min.css">
   <link rel="stylesheet" href="<?php echo base_url() ?>/css/global.css">
   <link rel="stylesheet" href="<?php echo base_url() ?>/css/home.css">
 
 </head>
+
 <body>
